3fd2f381c2adfe260c7499dd0dfc1604e10d9d1f
raptor <thulasitharan.gt96@gmail.com>
2020-01-16 09:48:52 +0530

    with pipeline command completed

--- src/main/scala/org/controller/deltaLakeEG/shellScriptSystemCommand.scala
+++ src/main/scala/org/controller/deltaLakeEG/shellScriptSystemCommand.scala
@@ -1,19 +1,46 @@
 package org.controller.deltaLakeEG
 
 import sys.process._
+import util.control.Breaks._
 object shellScriptSystemCommand {
   def main(args: Array[String]): Unit = {
-    val basePath="/home/raptor/IdeaProjects/SparkLearning/temp/"
-    val tableCreationBronze="sh "+basePath+"tableCreationBronze.sh"
-    val tableAppendingBronze_1="sh "+basePath+"tableAppendingBronze_1.sh"
-    val tableAppendingBronze_2="sh "+basePath+"tableAppendingBronze_2.sh"
-    val tableAppendingBronze_3="sh "+basePath+"tableAppendingBronze_3.sh"
-    val tableUpdationBronze_1="sh "+basePath+"tableUpdationBronze_1.sh"
-    val tableUpdationBronze_2="sh "+basePath+"tableUpdationBronze_2.sh"
-    val tableCreationSilver="sh "+basePath+"tableCreationSilver.sh"
-    val tableAppendingSilver_1="sh "+basePath+"tableAppendingSilver_1.sh"
+    val inputMap:collection.mutable.Map[String,String]=collection.mutable.Map[String,String]()
+    for (arg <- args)
+      {
+        val keyPart=arg.split("=",2)(0)
+        val valuePart=arg.split("=",2)(1)
+        inputMap.put(keyPart,valuePart)
+      }
+    //val basePath="/home/raptor/IdeaProjects/SparkLearning/temp/"
+    val basePath=inputMap("basePath")
+    val scriptNameList=inputMap("scriptNameList").split(",")
     var result:Int=2
-    result=tableCreationBronze!
+   try {
+     for (scriptName <- scriptNameList) {
+       println("executing script --------------> " + scriptName)
+       val commandToRun = "sh " + basePath + scriptName
+       result = commandToRun!;  // because of exclamation we need semi colon. else jar build fail's in REPL we don't need this
+       if (result == 0)
+         println(scriptName +" Excecution Success")
+       if (result != 0)
+         break
+       //"ls "+basePath!
+     }
+     println("Scripts executed Successfully .......................................... ")
+   }
+    catch {
+      case e:Exception => println(e.printStackTrace)
+    }
+    /* val tableCreationBronze="sh "+basePath+"tableCreationBronze.sh"
+     val tableAppendingBronze_1="sh "+basePath+"tableAppendingBronze_1.sh"
+     val tableAppendingBronze_2="sh "+basePath+"tableAppendingBronze_2.sh"
+     val tableAppendingBronze_3="sh "+basePath+"tableAppendingBronze_3.sh"
+     val tableUpdationBronze_1="sh "+basePath+"tableUpdationBronze_1.sh"
+     val tableUpdationBronze_2="sh "+basePath+"tableUpdationBronze_2.sh"
+     val tableCreationSilver="sh "+basePath+"tableCreationSilver.sh"
+     val tableAppendingSilver_1="sh "+basePath+"tableAppendingSilver_1.sh"*/
+   // var result:Int=2
+   // result=tableCreationBronze!
 
   }
 
