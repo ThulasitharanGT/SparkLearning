0ae11615ba2ff42075fa53bbb64b75e600142922
raptor <thulasitharan.gt96@gmail.com>
2020-08-02 13:18:03 +0530

    upgraded build to spark 2.4.5 and added multiple sources to one destination query

--- src/main/scala/org/util/kafka/kafkaUtilMethods.scala
+++ src/main/scala/org/util/kafka/kafkaUtilMethods.scala
@@ -14,3 +14,13 @@ object kafkaUtilMethods {
   def deleteTopic(adminZKClientConnection:AdminZkClient,topicName:String)=adminZKClientConnection.deleteTopic(topicName)
 
 }
+
+/*
+spark-submit --class org.util.kafka.createTopic  --num-executors 1 --executor-cores 2 --executor-memory 1g --driver-memory 512m --driver-cores 1 --packages org.apache.kafka:kafka-clients:2.3.1,org.apache.kafka:kafka_2.12:2.3.1,org.apache.spark:spark-sql-kafka-0-10_2.12:2.4.4 /home/raptor/IdeaProjects/SparkLearning/build/libs/SparkLearning-1.0-SNAPSHOT.jar  topicName=msgCheck topicPartitions=10 topicReplicationFactor=2 zookeeperIp=localhost:3039 isSecure=false sessionTimeOutMilliSecs=200000 connectionTimeOutMilliSecs=15000 maxInFlightRequests=20 metricGroup=myGroup metricType=myType
+
+spark-submit --class com.test.AtomicityUsingMysql.randomMsgProducer --driver-memory 512m --executor-memory 1g --num-executors 1 --executor-cores 2 --driver-cores 1 --packages org.apache.kafka:kafka-clients:2.3.1,org.apache.kafka:kafka_2.12:2.3.1 /home/raptor/IdeaProjects/KafkaGradleTest/build/libs/KafkaGradleTest-1.0-SNAPSHOT-all.jar key=check messageLength=5 numOfRecords=50 keySerializer=org.apache.kafka.common.serialization.StringSerializer valueSerializer=org.apache.kafka.common.serialization.StringSerializer bootStrapServer=localhost:9091,localhost:9092,localhost:9093 topicName=msgCheck
+
+
+spark-submit --class org.util.kafka.deleteTopic  --num-executors 1 --executor-cores 2 --executor-memory 1g --driver-memory 512m --driver-cores 1 --packages org.apache.kafka:kafka-clients:2.3.1,org.apache.kafka:kafka_2.12:2.3.1,org.apache.spark:spark-sql-kafka-0-10_2.12:2.4.4 /home/raptor/IdeaProjects/SparkLearning/build/libs/SparkLearning-1.0-SNAPSHOT.jar  topicName=msgCheck topicPartitions=10 topicReplicationFactor=2 zookeeperIp=localhost:3039 isSecure=false sessionTimeOutMilliSecs=200000 connectionTimeOutMilliSecs=15000 maxInFlightRequests=20 metricGroup=myGroup metricType=myType sleepMsForKafkaToDeleteTopic=60000
+
+ */
