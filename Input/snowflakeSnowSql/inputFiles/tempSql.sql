5dc8e3afec1bc6dd0ddb9ff8dbb4bf77f2656464
raptor <thulasitharan.gt96@gmail.com>
2021-07-30 11:54:36 +0530

    snow sql updates

---
+++ Input/snowflakeSnowSql/inputFiles/tempSql.sql
@@ -0,0 +1,3 @@
+put file:///home/raptor/Softwares/snowsql/inputFiles/src1.txt @"STREAM_TASK"."PUBLIC".%source_table;
+put file:///home/raptor/Softwares/snowsql/inputFiles/sec2.txt @"STREAM_TASK"."PUBLIC".%source_table;
+put file:///home/raptor/Softwares/snowsql/inputFiles/src1.json @~/inputFiles/;
